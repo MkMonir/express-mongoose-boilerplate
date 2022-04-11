e154551b7e751cf4144157e07022d01ab0bc97ae
MkMonir <khanmonir1335@gmail.com>
2022-04-11 17:49:06 +0600

    Middlewares and server headers

---
+++ app.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const monogoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const viewRouter = require('./routes/viewRoutes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+/////////// GLOBAL Middleware
+
+// note: SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
+
+// note: SET SECURITY HTTP HEADERS
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
+
+// note: DEVELOPMENT LOGGING
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// note: LIMIR REQUESTS FORM SAME API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 15 * 60 * 1000,
+  message: 'Too many requests from this ip, Please try again in an hour.',
+});
+app.use('/api', limiter);
+
+// note: BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// note: DATA SANITIZATION AGAINST NoSQL QUERY INJECTION
+app.use(monogoSanitize());
+
+// note: DATA SANITIZATION AGAINST XSS(XROSS-SITE SCRIPTING ATTACKS)
+app.use(xss());
+
+// note: PREVENT PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+// note: TEST MIDDLEWARE
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// note: Routes Middleware
+app.use('/', viewRouter);
+
+// Global error handling middleware
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
